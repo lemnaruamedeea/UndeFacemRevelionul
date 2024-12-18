76c7c0ea7d83901fc6c182c8f802a1567b8e6d61
Lemnaru Amedeea-Elena <lemnaruamedeeaelena@gmail.com>
2024-12-18 13:39:35 +0200

    meniuparty

---
+++ UFR Backend/UndeFacemRevelionul/ViewModels/ListMenusViewModel.cs
@@ -0,0 +1,12 @@
+﻿using UndeFacemRevelionul.Models;
+
+namespace UndeFacemRevelionul.ViewModels
+{
+    public class ListMenusViewModel
+    {
+        public int PartyId { get; set; } // ID-ul petrecerii
+        public List<FoodMenuModel> Menus { get; set; } // Lista meniurilor
+    }
+
+}
+
