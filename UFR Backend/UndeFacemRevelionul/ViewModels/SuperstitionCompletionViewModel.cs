9dec14cb10a44d399200233b64e73bcda9eaea6a
franci_v <franci.vlaicu@yahoo.com>
2025-02-04 22:32:27 +0200

    superstitii

---
+++ UFR Backend/UndeFacemRevelionul/ViewModels/SuperstitionCompletionViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace UndeFacemRevelionul.ViewModels
+{
+    public class SuperstitionCompletionViewModel
+    {
+        public int SuperstitionId { get; set; }
+        public int PartyId { get; set; }
+
+        // Câmpul pentru fișierul de imagine
+        public IFormFile Image { get; set; }
+    }
+
+}
